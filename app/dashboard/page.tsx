104345ff01980090b82f372f1a026e796b3430e2
Naveen Raj <naveenraj@poshmark.com>
2024-08-26 00:05:22 +0530

    Remove unused css

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 import { Content, Header, HeaderName, Theme } from '@carbon/react'
-import React from 'react'
 
 const Dashboard = () => {
     return (
@@ -10,7 +9,7 @@ const Dashboard = () => {
             </Header>
             <Content className='h-[calc(100dvh-3rem)] p-0'>
                 <div className="grid grid-cols-12 gap-4">
-                    <div className='col-span-2 p-4 border-2' style={{ borderColor: 'var(--cds-border-subtle-00)' }}>Category</div>
+                    <div className='col-span-2 p-4 border'>Category</div>
                     <div className='col-span-7 p-4 border'>Item</div>
                     <div className='col-span-3 p-4 border'>Cart</div>
                 </div>
