06c4d2a50dac46661257d1fb8ed404f804bf52c6
Naveen Raj <naveenraj@poshmark.com>
2024-08-30 14:19:26 +0530

    Add launch config for tauri

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
-      <p className="text-xl">Hello, Naveen!</p>
+      <p className="text-xl">Hello, World!</p>
+      <Link href={"/dashboard"}>Dashboard</Link>
+      <Link href={"/login"}>Login</Link>
     </div>
   );
 }
