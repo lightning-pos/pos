6b0b4ed347b2404c80d280139a2fd4205c4a0c65
Naveen Raj <naveenraj@poshmark.com>
2024-09-03 23:46:13 +0530

    Add orm support using kysely-driver

--- app/dash/pos/page.tsx
+++ app/dash/pos/page.tsx
@@ -1,9 +1,15 @@
 'use client'
+import { db } from '@/components/providers/system_provider'
 import { Content } from '@carbon/react'
 import { useQuery } from '@powersync/react'
+import { use } from 'react'
+import { uid } from 'uid'
 
 const POS = () => {
-  const { data: categories } = useQuery('select * from categories')
+  // use(db.insertInto('item_categories').values({ id: uid(), name: 'Steamed - ' + uid() }).execute())
+  // use(db.deleteFrom('item_categories').execute())
+  const categories = use(db.selectFrom('item_categories').selectAll().execute())
+
   return (
     <Content className='min-h-[calc(100dvh-3rem)] p-0'>
       <div className="grid grid-cols-12 gap-4 h-[calc(100dvh-3rem)]">
--- components/providers/system_provider.tsx
+++ components/providers/system_provider.tsx
@@ -1,22 +1,25 @@
 'use client';
 
-import { AppSchema } from '@/lib/powersync/app_schema';
+import { AppSchema, Database } from '@/lib/powersync/app_schema';
 import { Connector } from '@/lib/powersync/connector';
 import { PowerSyncContext } from '@powersync/react';
 import { PowerSyncDatabase } from '@powersync/web';
 import { Loading } from '@carbon/react';
 import Logger from 'js-logger';
 import React, { Suspense } from 'react';
+import { wrapPowerSyncWithKysely } from '@powersync/kysely-driver';
 
 // eslint-disable-next-line react-hooks/rules-of-hooks
 Logger.useDefaults();
 Logger.setLevel(Logger.DEBUG);
 
-const powerSync = new PowerSyncDatabase({
+const powerSyncDb = new PowerSyncDatabase({
   database: { dbFilename: 'powersync2.db' },
   schema: AppSchema,
   flags: { disableSSRWarning: false }
 });
+
+export const db = wrapPowerSyncWithKysely<Database>(powerSyncDb);
 
 // TODO: Enable once the backend for connector is ready
 // const connector = new Connector();
@@ -25,7 +28,7 @@ const powerSync = new PowerSyncDatabase({
 export const SystemProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <Suspense fallback={<Loading />}>
-      <PowerSyncContext.Provider value={powerSync}>{children}</PowerSyncContext.Provider>
+      <PowerSyncContext.Provider value={powerSyncDb}>{children}</PowerSyncContext.Provider>
     </Suspense>
   );
 };
