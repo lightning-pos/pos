0580418e6480201edc0132177750f617df22fe8d
Naveen Raj <naveenraj@poshmark.com>
2024-09-07 18:03:34 +0530

    Add layout for pos page items

--- app/dash/pos/page.tsx
+++ app/dash/pos/page.tsx
@@ -1,26 +1,60 @@
 'use client'
 import { db } from '@/components/providers/system_provider'
-import { Content } from '@carbon/react'
-import { use } from 'react'
+import { Category, Item } from '@/lib/powersync/app_schema'
+import { AspectRatio, ClickableTile, Column, Content, Grid } from '@carbon/react'
+import { useState, useEffect } from 'react'
+import { Corn } from '@carbon/icons-react'
 
 const POS = () => {
-  const categories = use(db.selectFrom('item_categories').selectAll().execute())
+  const [categories, setCategories] = useState<Array<Category>>([])
+  const [selectedCategory, setSelectedCategory] = useState<Category | null>(null)
+  const [items, setItems] = useState<Array<Item>>([])
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const result = await db.selectFrom('item_categories').selectAll().execute()
+      setCategories(result)
+    }
+    fetchCategories()
+  }, [])
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      if (selectedCategory) {
+        const result = await db
+          .selectFrom('items')
+          .selectAll()
+          .where('item_category_id', '=', selectedCategory.id)
+          .execute()
+        setItems(result)
+      }
+    }
+    fetchItems()
+  }, [selectedCategory])
 
   return (
-    <Content className='min-h-[calc(100dvh-3rem)] p-0'>
-      <div className="grid grid-cols-12 gap-4 h-[calc(100dvh-3rem)]">
-        <div className='col-span-2 p-4 border'>
-          Category
-          <br />
-          {
-            categories.map((category) => (
-              <div key={category.id}>{category.name}<br /></div>
-            ))
-          }
-        </div>
-        <div className='col-span-7 p-4 border'>Item</div>
-        <div className='col-span-3 p-4 border'>Cart</div>
-      </div>
+    <Content className='min-h-[calc(100dvh-3rem)] p-0 pt-4'>
+      <Grid fullWidth className='p-0'>
+        <Column lg={2} className='m-0 p-0'>
+          {categories.map((category) => (
+            <ClickableTile key={category.id} onClick={() => setSelectedCategory(category)}>
+              {category.name}
+            </ClickableTile>
+          ))}
+        </Column>
+        <Column lg={10} className='px-4'>
+          <Grid narrow>
+            {items.map((item) => (
+              <Column key={item.id} lg={2} className='mb-4'>
+                <ClickableTile renderIcon={Corn}>
+                  <AspectRatio ratio='3x2'>{item.name}</AspectRatio>
+                </ClickableTile>
+              </Column>
+            ))}
+          </Grid>
+        </Column>
+        <Column lg={4} className='m-0 p-0'>Cart</Column>
+      </Grid>
     </Content>
   )
 }
