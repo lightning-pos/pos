9e8bab884a19eb5ed38bcdcc9aae2aeb3552d863
Naveen Raj K <rajnaveen344@gmail.com>
2024-10-01 14:38:52 +0530

    Add migration support for tauri

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,20 @@
+use tauri_plugin_sql::{Migration, MigrationKind};
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let migrations = vec![Migration {
+        version: 1,
+        description: "Initial migration",
+        sql: include_str!("../migrations/0000_wise_ultron.sql"),
+        kind: MigrationKind::Up,
+    }];
+
     tauri::Builder::default()
-        .plugin(tauri_plugin_sql::Builder::default().build())
+        .plugin(
+            tauri_plugin_sql::Builder::default()
+                .add_migrations("sqlite:minnal.db", migrations)
+                .build(),
+        )
         .plugin(tauri_plugin_fs::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
