79df96dfee4654adac329bc7639584b94cdbc594
Naveen Raj K <rajnaveen344@gmail.com>
2025-01-26 20:19:02 +0530

    add orders and customers to schema.rs

--- src-tauri/src/schema.rs
+++ src-tauri/src/schema.rs
@@ -66,6 +66,60 @@ table! {
     }
 }
 
+table! {
+    customers (id) {
+        id -> Text,
+        name -> Nullable<Text>,
+        email -> Nullable<Text>,
+        country_code -> Nullable<Text>,
+        phone_number -> Nullable<Text>,
+        created_at -> BigInt,
+        updated_at -> BigInt,
+    }
+}
+
+table! {
+    orders (id) {
+        id -> Text,
+        customer_id -> Text,
+        customer_name -> Text,
+        customer_phone_number -> Text,
+        order_date -> BigInt,
+        net_amount -> BigInt,
+        disc_amount -> BigInt,
+        taxable_amount -> BigInt,
+        tax_amount -> BigInt,
+        total_amount -> BigInt,
+        state -> Text,
+        created_at -> BigInt,
+        updated_at -> BigInt,
+    }
+}
+
+table! {
+    order_items (id) {
+        id -> Text,
+        order_id -> Text,
+        item_id -> Text,
+        item_name -> Text,
+        quantity -> BigInt,
+        price_amount -> BigInt,
+        tax_amount -> BigInt,
+        created_at -> BigInt,
+        updated_at -> BigInt,
+    }
+}
+
+// ManyToOne (items, item_categories)
 joinable!(items -> item_categories (category_id));
+
+// ManyToMany (items, item_taxes)
 joinable!(item_taxes -> items (item_id));
 joinable!(item_taxes -> taxes (tax_id));
+
+// ManyToOne (orders, customers)
+joinable!(orders -> customers (customer_id));
+
+// ManyToMany (orders, order_items)
+joinable!(order_items -> orders (order_id));
+joinable!(order_items -> items (item_id));
